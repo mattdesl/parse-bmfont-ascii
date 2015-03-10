000efefadeeb2436db68fc066c1ab93261296783
mattdesl <dave.des@gmail.com>
2015-03-10 16:00:58 -0400

    adding test for pages

--- test/test.js
+++ test/test.js
@@ -20,3 +20,18 @@ test('should parse fnt as object', function(t) {
     t.equal(result.kernings.length, 487, 'kernings parsed')  
   })
 })
+
+test('should handle multipage fonts correctly', function(t) {
+  t.plan(4)
+  var file = path.join(__dirname, 'Nexa Light-32-Multi.fnt')
+  fs.readFile(file, function(err, data) {
+    if (err)
+      t.fail(err)
+
+    var result = parse(data)
+    t.equal(result.info.face, 'Nexa Light', 'face parsed')
+    t.equal(result.chars.length, 96, 'chars parsed')
+    t.equal(result.kernings.length, 487, 'kernings parsed')  
+    t.deepEqual(result.pages, [ 'sheet_0.png', 'sheet_1.png' ], 'parses pages')  
+  })
+})
